fdbd60f8873aff8a93295cf50c1a1e025386e9d8
Vasiliauskas Antanas <antanas.vasiliauskas@mif.stud.vu.lt>
2025-05-12 10:46:23 +0300

    cleaning and housekeeping

--- src/main/java/com/example/MyBatisSessionFactoryProvider.java
+++ src/main/java/com/example/MyBatisSessionFactoryProvider.java
@@ -1,20 +1,17 @@
 package com.example;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
 import jakarta.ejb.Singleton;
 import jakarta.ejb.Startup;
 import jakarta.enterprise.inject.Produces;
+import jakarta.annotation.PostConstruct;
+
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.*;
 
-import java.io.IOException;
 import java.io.InputStream;
 
 @Startup
@@ -25,40 +22,28 @@ public class MyBatisSessionFactoryProvider {
 
     @PostConstruct
     public void init() {
-        try {
-            InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-            // inject JNDI datasource
-            try {
-                DataSource ds = (DataSource) new InitialContext().lookup("jdbc/UniversityDS");
-                sqlSessionFactory.getConfiguration().setEnvironment(
-                    new org.apache.ibatis.mapping.Environment(
-                        "payara",
-                        sqlSessionFactory.getConfiguration().getEnvironment().getTransactionFactory(),
-                        ds
-                    )
-                );
-            } catch (NamingException e) {
-                throw new RuntimeException("Failed to lookup JNDI DataSource", e);
-            }
-
-        } catch (IOException e) {
-            throw new RuntimeException("Could not load mybatis-config.xml", e);
+        try (InputStream input = Resources.getResourceAsStream("mybatis-config.xml")) {
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(input);
+            DataSource ds = (DataSource) new InitialContext().lookup("jdbc/UniversityDS");
+            Environment env = new Environment(
+                "payara",
+                sqlSessionFactory.getConfiguration().getEnvironment().getTransactionFactory(),
+                ds
+            );
+            sqlSessionFactory.getConfiguration().setEnvironment(env);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to initialize MyBatis", e);
         }
     }
 
     @Produces
-    public SqlSessionFactory produceSessionFactory() {
+    public SqlSessionFactory getFactory() {
         return sqlSessionFactory;
     }
 
     @Produces
-    public SqlSession produceSession() {
-        return sqlSessionFactory.openSession(true); // auto-commit
-    }
-
-    @PreDestroy
-    public void cleanup() {
-        // nothing to clean in default MyBatis SqlSessionFactory
+    public SqlSession getSession() {
+        return sqlSessionFactory.openSession(true);
     }
 }
+
